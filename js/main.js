327487c3d79a422e42c7981f9457992a337003be
sinsit1 <alexandra8989@gmail.com>
2018-07-12 17:15:51 +0200

    fix refresh

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@
 
 var body = document.querySelector('body');
 var buttonSearch = document.querySelector('.buttonSearch');
+var container = document.querySelector('.container');
 var searchHolder = document.getElementById('buscador');
 var element;
 var list = document.createElement('ul');
 var h2;
 var result;
+var noResults;
 
 function favorites(event) {
   var targetElement = event.currentTarget;
@@ -17,14 +19,12 @@ function favorites(event) {
   }
 }
 
-
-
 function searchShow() {
   list.innerHTML = ''; // reseteo la busqueda
   var search = searchHolder.value;
   list = document.createElement('ul');
 
-  body.appendChild(list);
+
 
   fetch('http://api.tvmaze.com/search/shows?q=' + search)
     .then(function (response) {
@@ -36,12 +36,14 @@ function searchShow() {
 
       //controlo que si no existen resultados me avise
       if (result.length === 0) {
-        var noResults= document.createElement('h2');
+        container.innerHTML='';
+        noResults= document.createElement('h2');
         noResults.innerHTML='No existen resultados para su búsqueda';
-        body.appendChild(noResults);
+        container.appendChild(noResults);
 
       } else { //si hay resultados....
-
+        container.innerHTML='';
+        container.appendChild(list);
         for (var i = 0; i < result.length; i++) {
           element = document.createElement('li');
           element.classList.add('list');
@@ -64,12 +66,9 @@ function searchShow() {
           list.classList.add('grid-container-results');
           element.appendChild(img);
           element.appendChild(h2);
-
-
         }
       }
     });
-
 }
 
 // principio de código para capturar ENTER
@@ -80,5 +79,3 @@ searchHolder.addEventListener('keyup', function (e) {
 });
 
 buttonSearch.addEventListener('click', searchShow);
-
-///////////// NOTA. CONTROLAR QUÉ PASARÍA SI NO EXISTIERA LA SERIE. aÑADIR MENSAJE 404/////////////////
